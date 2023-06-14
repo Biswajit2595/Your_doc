9ddc210cee1611418db0648fd2b33564dc6f63b4
Biswajit Roy <broy16431@gmail.com>
2023-06-14 23:47:43 +0530

    day-1

---
+++ yourdoc/src/Pages/LabTest.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LabTest() {
+  return (
+    <div>LabTest</div>
+  )
+}
+
+export default LabTest;
---
+++ yourdoc/src/Routes/AllRoutes.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Medicine from '../Pages/Medicine'
+import Healthcare from '../Pages/Healthcare'
+import HealthDevices from '../Pages/HealthDevices'
+import LabTest from '../Pages/LabTest'
+import Doctors from '../Pages/Doctors';
+import Appointments from '../Pages/Appointments'
+import Store from '../Pages/Store'
+import Blogs from '../Pages/Blogs'
+
+function AllRoutes() {
+  return (
+    <div>
+        <Routes>
+            <Route path='/medicine' element={ <Medicine/> } />
+            <Route path='/healthcare' element={ <Healthcare/> } />
+            <Route path='/healthdevices' element={ <HealthDevices/> } />
+            <Route path='/labtests' element={ <LabTest /> } />
+            <Route path='/doctors' element={ <Doctors /> } />
+            <Route path='/appointments' element={ <Appointments /> } />
+            <Route path='/stores' element={ <Store /> } />
+            <Route path='/blog' element={ <Blogs /> } />
+        </Routes>
+    </div>
+  )
+}
+
+export default AllRoutes
